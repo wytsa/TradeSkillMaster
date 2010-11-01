2e49264a1a4d4ca09bb9b426b000f3a0a2de4579
Mischanix <mischanix@curseforge.net>
2010-11-01 02:20:56 +0000

    Status page gui base
    git-svn-id: svn://svn.curseforge.net/wow/tradeskill-master/mainline/trunk@27 95b459c4-dbbf-4912-a75f-4f35827d8608

--- TradeSkillMaster.lua
+++ TradeSkillMaster.lua
@@ -199,6 +199,7 @@ function TSM:BuildIcons()
 			frame:SetScript("OnClick", function()
 					if #(TSM.Frame.children) > 0 then
 						TSM.Frame:ReleaseChildren()
+						TSM.Frame:SetStatusText("")
 					end
 					private.icons[i].loadGUI(TSM.Frame)
 				end)
@@ -242,14 +243,58 @@ function TSM:DefaultContent()
 	local function LoadGUI(parent)
 		-- Create the main tree-group that will control and contain the entire TSM
 		local content = AceGUI:Create("SimpleGroup")
-		content:SetLayout("list")
+		content:SetLayout("flow")
 		parent:AddChild(content)
 		
 		local text = AceGUI:Create("Label")
-		text:SetText("This is a test module!!!")
+		text:SetText("Status")
 		text:SetFullWidth(true)
 		text:SetFontObject(GameFontNormalHuge)
+		
 		content:AddChild(text)
+		local fakeModules = {}
+		local authors = {"Sapu","Sapu94","jnt","xubera"}
+		for i=1,10 do
+			tinsert(fakeModules, {name="TradeSkillMaster_"..i, version="r1"..i, authors=authors[(i%4)+1], desc="This module does stuff to the "..i.."th degree"})
+		end
+		for i,module in pairs(fakeModules) do
+		
+			local thisFrame = AceGUI:Create("SimpleGroup")
+			thisFrame:SetRelativeWidth(0.5)
+			thisFrame:SetLayout("list")
+			
+			local name = AceGUI:Create("Label")
+			name:SetText("\124cffbde3dbModule: \124r"..module.name)
+			name:SetFullWidth(true)
+			name:SetFontObject(GameFontNormalLarge)
+			
+			local version = AceGUI:Create("Label")
+			version:SetText("\124cffbde3dbVersion: \124r"..module.version)
+			version:SetFullWidth(true)
+			version:SetFontObject(GameFontNormal)
+			
+			local authors = AceGUI:Create("Label")
+			authors:SetText("\124cffbde3dbAuthor(s): \124r"..module.authors)
+			authors:SetFullWidth(true)
+			authors:SetFontObject(GameFontNormal)
+			
+			local desc = AceGUI:Create("Label")
+			desc:SetText("\124cffbde3dbDescription: \124r"..module.desc)
+			desc:SetFullWidth(true)
+			desc:SetFontObject(GameFontNormal)
+			
+			local spacer = AceGUI:Create("Heading")
+			spacer:SetText("")
+			spacer:SetFullWidth(true)
+			
+			thisFrame:AddChild(spacer)
+			thisFrame:AddChild(name)
+			thisFrame:AddChild(version)
+			thisFrame:AddChild(authors)
+			thisFrame:AddChild(desc)
+			content:AddChild(thisFrame)
+		end
+
 	end
 	
 	lib:RegisterIcon(name, icon, LoadGUI)
