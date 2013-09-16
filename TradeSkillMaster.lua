8e0c8c6a2dbb21c7ff3b2a263bb1392b3bb2a91c
Bart39 <bart39@sky.com>
2013-09-16 00:58:08 +0000

    added disenchant mats to tooltip
    git-svn-id: svn://svn.curseforge.net/wow/tradeskill-master/mainline/trunk@955 95b459c4-dbbf-4912-a75f-4f35827d8608

--- TradeSkillMaster.lua
+++ TradeSkillMaster.lua
@@ -40,7 +40,7 @@ TSM.designDefaults = {
 		category = { 36, 106, 36, 1 },
 		category2 = { 85, 180, 8, 1 },
 		tooltip = { 130, 130, 250, 1 },
-		advanced = {  255, 30, 0, 1 },
+		advanced = { 255, 30, 0, 1 },
 	},
 	edgeSize = 1.5,
 	fonts = {
@@ -143,7 +143,7 @@ function TSM:OnInitialize()
 	TSM.db.factionrealm.accountKey = TSM.db.factionrealm.accountKey or (GetRealmName() .. random(time()))
 	-- add this character to the list of characters on this realm
 	TSM.db.factionrealm.characters[UnitName("player")] = true
-	
+
 	if not TSM.db.profile.design then
 		TSM:LoadDefaultDesign()
 	end
@@ -188,7 +188,7 @@ function TSM:OnInitialize()
 
 	-- create the main TSM frame
 	TSM:CreateMainFrame()
-	
+
 	-- fix any items with spaces in them
 	for itemString, groupPath in pairs(TSM.db.profile.items) do
 		if strfind(itemString, " ") then
@@ -335,6 +335,29 @@ function TSM:GetTooltip(itemString, quantity)
 						tinsert(text, { left = "  " .. L["Disenchant Value:"], right = TSMAPI:FormatTextMoney(deValue, "|cffffffff", true) })
 					end
 				end
+				local _, itemLink, quality, ilvl, _, iType = TSMAPI:GetSafeItemInfo(itemString)
+				local itemString = TSMAPI:GetItemString(itemLink)
+				local WEAPON, ARMOR = GetAuctionItemClasses()
+
+				for _, data in ipairs(TSMAPI.DisenchantingData.disenchant) do
+					for item, itemData in pairs(data) do
+						if item ~= "desc" and itemData.itemTypes[iType] and itemData.itemTypes[iType][quality] then
+							for _, deData in ipairs(itemData.itemTypes[iType][quality]) do
+								if ilvl >= deData.minItemLevel and ilvl <= deData.maxItemLevel then
+									local matValue = TSM:GetCustomPrice(TSM.db.profile.deValueSource, item)
+									local value = (matValue or 0) * deData.amountOfMats
+									if value > 0 then
+										if moneyCoinsTooltip then
+											tinsert(text, { left = "    " .. TSMAPI:GetSafeItemInfo(item) .. " x " .. deData.amountOfMats, right = TSMAPI:FormatTextMoneyIcon(value, "|cffffffff", true) })
+										else
+											tinsert(text, { left = "    " .. TSMAPI:GetSafeItemInfo(item) .. " x " .. deData.amountOfMats, right = TSMAPI:FormatTextMoney(value, "|cffffffff", true) })
+										end
+									end
+								end
+							end
+						end
+					end
+				end
 			end
 		end
 	end
@@ -451,10 +474,10 @@ end
 
 function TSMAPI:GetChatFrame()
 	local chatFrame = DEFAULT_CHAT_FRAME
-	for i=1, NUM_CHAT_WINDOWS do
+	for i = 1, NUM_CHAT_WINDOWS do
 		local name = strlower(GetChatWindowInfo(i) or "")
 		if name ~= "" and name == strlower(TSM.db.global.chatFrame) then
-			chatFrame = _G["ChatFrame"..i]
+			chatFrame = _G["ChatFrame" .. i]
 			break
 		end
 	end
