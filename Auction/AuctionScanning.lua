eef85b6d3198a22461869f683dee8f270ea2ee15
Bart39 <bart39@sky.com>
2013-12-24 03:10:32 +0000

    fix to the previous fix
    git-svn-id: svn://svn.curseforge.net/wow/tradeskill-master/mainline/trunk@1074 95b459c4-dbbf-4912-a75f-4f35827d8608

--- Auction/AuctionScanning.lua
+++ Auction/AuctionScanning.lua
@@ -170,10 +170,10 @@ function TSMAPI.AuctionScan:ScanLastPage(callbackHandler)
 	SortAuctionClearSort("list")
 	
 	-- setup the query
+	private.query = {name="", page=0}
 	private.query.timeDelay = 0 -- a delay used to wait for information to show up
 	private.query.retries = 0 -- how many times we've done a hard retry so far
 	private.query.hardRetry = nil -- if a page hasn't loaded after we've tried a delay, we'll do a hard retry and re-send the query
-	private.query = {name="", page=0}
 
 	-- setup other stuff
 	wipe(private.data)
