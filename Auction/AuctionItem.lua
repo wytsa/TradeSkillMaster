fa91bab6b8f2d56fd798cc1339ad1167e2b777df
Sapu94 <sapu94@gmail.com>
2014-03-29 17:40:50 +0000

    Fixed bug with uniqueIds for battle pets which was causing errors in TSM_Shopping.
    git-svn-id: svn://svn.curseforge.net/wow/tradeskill-master/mainline/trunk@1152 95b459c4-dbbf-4912-a75f-4f35827d8608

--- Auction/AuctionItem.lua
+++ Auction/AuctionItem.lua
@@ -176,7 +176,11 @@ local AuctionItem = {
 	AddAuctionRecord = function(self, ...)
 		local record = NewRecord()
 		record:SetData(self, ...)
-		record.uniqueID = select(9, (":"):split(self.itemLink))
+		if strfind(self.itemLink, "battlepet") then
+			record.uniqueID = table.concat({TSMAPI:Select({2, 3, 4, 5, 6, 7}, (":"):split(self.itemLink))}, ".")
+		else
+			record.uniqueID = select(9, (":"):split(self.itemLink))
+		end
 		self:AddRecord(record)
 	end,
 	
