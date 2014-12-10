29ecd632b462ded6218f48af1c69404a7d948f0c
Sapu94 <sapu94@gmail.com>
2014-12-09 22:05:20 -0800

    Working on new debug logger.

--- Core/DebugLogging.lua
+++ Core/DebugLogging.lua
@@ -11,12 +11,120 @@
 local TSM = select(2, ...)
 local DebugLogging = TSM:NewModule("DebugLogging")
 local L = LibStub("AceLocale-3.0"):GetLocale("TradeSkillMaster") -- loads the localization table
-local private = {}
+local private = {startTime=(debugprofilestop() - (5 * 60 * 60 * 1000)), logs={}}
 local embeds = {"LOG_TRACE", "LOG_DEBUG", "LOG_INFO", "LOG_WARNING", "LOG_ERROR"}
 
 function DebugLogging:Embed(obj)
+	local name = TSM.Modules:GetName(obj)
 	for key, func in pairs(private.embeds) do
 		obj[key] = func
+	end
+	private.logs[TSM.Modules:GetName(obj)] = {}
+end
+
+function private:CreateViewer()
+	if private.frame then return end
+	
+	local frameInfo = {
+		type = "Frame",
+		hidden = true,
+		widget = private.frame,
+		strata = "DIALOG",
+		size = {700, 500},
+		points = {{"CENTER"}},
+		scripts = {"OnMouseDown", "OnMouseUp"},
+		children = {
+			{
+				type = "Text",
+				text = format("TSM Debug Log Viewer"),
+				textFont = {TSMAPI.Design:GetContentFont(), 18},
+				points = {{"TOP", "", 0, -3}},
+			},
+			{
+				type = "VLine",
+				offset = 0,
+				size = {2, 25},
+				points = {{"TOPRIGHT", -25, -1}},
+			},
+			{
+				type = "Button",
+				key = "closeBtn",
+				text = "X",
+				textHeight = 18,
+				size = {19, 19},
+				points = {{"TOPRIGHT", -3, -3}},
+				scripts = {"OnClick"},
+			},
+			{
+				type = "HLine",
+				offset = -24,
+			},
+			{
+				type = "Text",
+				text = "This is where some filters and such will go...",
+				points = {{"CENTER", "", "TOP", 0, -39}},
+			},
+			{
+				type = "HLine",
+				offset = -54,
+			},
+			{
+				type = "ScrollingTableFrame",
+				key = "logST",
+				headFontSize = 14,
+				stCols = {{name="Time", width=0.11, align="RIGHT"}, {name="Module", width=0.1, align="CENTER"}, {name="Severity", width=0.1, align="CENTER"}, {name="Caller", width=0.2}, {name="Message", width=0.5}},
+				stDisableSelection = true,
+				points = {{"TOPLEFT", 5, -60}, {"BOTTOMRIGHT", "", -5, 5}},
+			},
+		},
+		handlers = {
+			OnMouseDown = function(self)
+				self:StartMoving()
+			end,
+			OnMouseUp = function(self)
+				self:StopMovingOrSizing()
+			end,
+			closeBtn = {
+				OnClick = function(self)
+					self:GetParent():Hide()
+				end,
+			},
+		},
+	}
+	local frame = TSMAPI:BuildFrame(frameInfo)
+	TSMAPI.Design:SetFrameBackdropColor(frame)
+	frame:SetMovable(true)
+	frame:SetScale(UIParent:GetScale())
+	private.frame = frame
+end
+
+function TSMAPI.Debug:ShowLogViewer()
+	private:CreateViewer()
+	private.frame:Show()
+	
+	-- update ST data
+	local stData = {}
+	for module, logs in pairs(private.logs) do
+		for i, logInfo in ipairs(logs) do
+			tinsert(stData, {
+				cols = {
+					{value = format("%.3f", logInfo.timestamp)},
+					{value = logInfo.module},
+					{value = logInfo.severity},
+					{value = logInfo.caller},
+					{value = logInfo.msg},
+				},
+				timestamp = logInfo.timestamp,
+			})
+		end
+	end
+	sort(stData, function(a, b) return a.timestamp < b.timestamp end)
+	private.frame.logST:SetData(stData)
+end
+
+function TSMAPI.Debug:PrintLogs(module)
+	for _, data in ipairs(private.logs[module]) do
+		print(format("[%10.3f] [%s] %s - %s: %s", data.timestamp, data.severity, data.module, data.caller, data.msg))
 	end
 end
 
@@ -29,10 +137,10 @@ function private.LOG(module, severity, ...)
 			args[i] = tostring(args[i])
 		end
 	end
-	local caller = strmatch(debugstack(3, 1, 0), "[A-Za-z]+\.lua:[0-9]+")
-	local module = "TSM_Crafting"
-	local timeDiff = floor(debugprofilestop()-private.startTime2) / 1000
-	print(format("[%10.3f] [%s] %s - %s: %s", timeDiff, severity, module, caller, format(unpack(args))))
+	local caller = strmatch(debugstack(3, 1, 0), "[A-Za-z_]+\.lua:[0-9]+")
+	caller = gsub(caller, "TradeSkillMaster", "TSM")
+	local timestamp = floor(debugprofilestop()-private.startTime) / 1000
+	tinsert(private.logs[module], {severity=severity, module=module, caller=caller, timestamp=timestamp, msg=format(unpack(args))})
 end
 
 private.embeds = {
