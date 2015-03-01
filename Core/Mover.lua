6d1c000d073af21c3eff67dc69751eb94cb59d8e
Bart39 <bart39@sky.com>
2015-03-01 00:44:20 +0000

    update Bagnon / Add LiteBag support

--- Core/Mover.lua
+++ Core/Mover.lua
@@ -659,7 +659,11 @@ function TSMAPI:MoveItems(requestedItems, callback, includeSoulbound)
 end
 
 function TSM:areBanksVisible()
-	if BagnonFrameguildbank and BagnonFrameguildbank:IsVisible() then
+	if BagnonFrameGuildBank and BagnonFrameGuildBank:IsVisible() then
+		return true
+	elseif BagnonFrameguild and BagnonFrameguild:IsVisible() then
+		return true
+	elseif BagnonFrameinventory and BagnonFrameinventory:IsVisible() then
 		return true
 	elseif BagnonFramebank and BagnonFramebank:IsVisible() then
 		return true
@@ -702,6 +706,10 @@ function TSM:areBanksVisible()
 	elseif DufUIBank and DufUIBank:IsVisible() then
 		return true
 	elseif SVUI_BankContainerFrame and SVUI_BankContainerFrame:IsVisible() then
+		return true
+	elseif LiteBagBank and LiteBagBank:IsVisible() then
+		return true
+	elseif LiteBagInventory and LiteBagInventory:IsVisible() then
 		return true
 	end
 	return nil
