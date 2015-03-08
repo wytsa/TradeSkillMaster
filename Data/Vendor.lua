b0e148cedd8398813dcc39ae895aec1cc2c51bb3
Sapu94 <sapu94@gmail.com>
2015-03-08 12:19:24 -0700

    Updated list of vendor-bought items and changed vendor code over to new itemString format.

--- Data/Vendor.lua
+++ Data/Vendor.lua
@@ -12,83 +12,118 @@ local L = LibStub("AceLocale-3.0"):GetLocale("TradeSkillMaster") -- loads the lo
 
 function Vendor:OnEnable()
 	Vendor:RegisterEvent("MERCHANT_SHOW", "ScanMerchant")
+	local itemString = next(TSM.db.global.vendorItems)
+	if itemString and TSMAPI:GetItemString2(itemString) ~= itemString then
+		-- they just upgraded to TSM3, so wipe the table
+		wipe(TSM.db.global.vendorItems)
+	end
 end
 
 local vendorItems = {
-	["item:2320:0:0:0:0:0:0"] = 10,
-	["item:2321:0:0:0:0:0:0"] = 10,
-	["item:2324:0:0:0:0:0:0"] = 25,
-	["item:2325:0:0:0:0:0:0"] = 1000,
-	["item:2604:0:0:0:0:0:0"] = 50,
-	["item:2605:0:0:0:0:0:0"] = 10,
-	["item:2678:0:0:0:0:0:0"] = 10,
-	["item:2880:0:0:0:0:0:0"] = 100,
-	["item:3371:0:0:0:0:0:0"] = 100,
-	["item:3466:0:0:0:0:0:0"] = 2000,
-	["item:4289:0:0:0:0:0:0"] = 50,
-	["item:4291:0:0:0:0:0:0"] = 500,
-	["item:4340:0:0:0:0:0:0"] = 350,
-	["item:4341:0:0:0:0:0:0"] = 500,
-	["item:4342:0:0:0:0:0:0"] = 2500,
-	["item:4399:0:0:0:0:0:0"] = 200,
-	["item:4400:0:0:0:0:0:0"] = 2000,
-	["item:4470:0:0:0:0:0:0"] = 38,
-	["item:6260:0:0:0:0:0:0"] = 50,
-	["item:6261:0:0:0:0:0:0"] = 100,
-	["item:8343:0:0:0:0:0:0"] = 2000,
-	["item:10290:0:0:0:0:0:0"] = 2500,
-	["item:10647:0:0:0:0:0:0"] = 2000,
-	["item:10648:0:0:0:0:0:0"] = 100,
-	["item:11291:0:0:0:0:0:0"] = 4500,
-	["item:14341:0:0:0:0:0:0"] = 5000,
-	["item:17020:0:0:0:0:0:0"] = 1000,
-	["item:17194:0:0:0:0:0:0"] = 10,
-	["item:17196:0:0:0:0:0:0"] = 50,
-	["item:30817:0:0:0:0:0:0"] = 25,
-	["item:34412:0:0:0:0:0:0"] = 1000,
-	["item:35949:0:0:0:0:0:0"] = 8500,
-	["item:38426:0:0:0:0:0:0"] = 30000,
-	["item:38682:0:0:0:0:0:0"] = 1000,
-	["item:39354:0:0:0:0:0:0"] = 15,
-	["item:39501:0:0:0:0:0:0"] = 1200,
-	["item:39502:0:0:0:0:0:0"] = 5000,
-	["item:39684:0:0:0:0:0:0"] = 9000,
-	["item:40533:0:0:0:0:0:0"] = 50000,
-	["item:44835:0:0:0:0:0:0"] = 10,
-	["item:44853:0:0:0:0:0:0"] = 25,
-	["item:52188:0:0:0:0:0:0"] = 15000,
-	["item:58274:0:0:0:0:0:0"] = 11000,
-	["item:58278:0:0:0:0:0:0"] = 16000,
-	["item:62323:0:0:0:0:0:0"] = 60000,
-	["item:62786:0:0:0:0:0:0"] = 1000,
-	["item:62787:0:0:0:0:0:0"] = 1000,
-	["item:62788:0:0:0:0:0:0"] = 1000,
-	["item:67319:0:0:0:0:0:0"] = 328990,
-	["item:67335:0:0:0:0:0:0"] = 445561,
-	["item:67348:0:0:0:0:0:0"] = 394755,
-	["item:68047:0:0:0:0:0:0"] = 170437,
-	["item:74659:0:0:0:0:0:0"] = 30000,
-	["item:74660:0:0:0:0:0:0"] = 15000,
-	["item:74832:0:0:0:0:0:0"] = 12000,
-	["item:74845:0:0:0:0:0:0"] = 35000,
-	["item:74851:0:0:0:0:0:0"] = 14000,
-	["item:74852:0:0:0:0:0:0"] = 16000,
-	["item:74854:0:0:0:0:0:0"] = 7000,
-	["item:79740:0:0:0:0:0:0"] = 23,
-	["item:83092:0:0:0:0:0:0"] = 20000.0000,
-	["item:85583:0:0:0:0:0:0"] = 12000,
-	["item:85584:0:0:0:0:0:0"] = 17000,
-	["item:85585:0:0:0:0:0:0"] = 27000,
+	["i:80433"] = 2000000, -- Blood Spirit
+	["i:83092"] = 200000000, -- Orb of Mystery
+	["i:65893"] = 30000000, -- Sands of Time
+	["i:58265"] = 20000, -- Highland Pomegranate
+	["i:65892"] = 50000000, -- Pyrium-Laced Crystalline Vial
+	["i:67319"] = 328990, -- Preserved Ogre Eye
+	["i:67335"] = 445561, -- Silver Charm Bracelet
+	["i:74659"] = 30000, -- Farm Chicken
+	["i:74660"] = 15000, -- Pandaren Peach
+	["i:74832"] = 12000, -- Barley
+	["i:74845"] = 35000, -- Ginseng
+	["i:74851"] = 14000, -- Rice
+	["i:74852"] = 16000, -- Yak Milk
+	["i:74854"] = 7000, -- Instant Noodles
+	["i:85583"] = 12000, -- Needle Mushrooms
+	["i:85584"] = 17000, -- Silkworm Pupa
+	["i:85585"] = 27000, -- Red Beans
+	["i:102539"] = 5000, -- Fresh Strawberries
+	["i:102540"] = 5000, -- Fresh Mangos
+	["i:52188"] = 15000, -- Jeweler's Setting
+	["i:62323"] = 60000, -- Deathwing Scale Fragment
+	["i:43102"] = 750000, -- Frozen Orb
+	["i:44499"] = 30000000, -- Salvaged Iron Golem Parts
+	["i:44500"] = 15000000, -- Elementium-Plated Exhaust Pipe
+	["i:44501"] = 10000000, -- Goblin-Machined Piston
+	["i:45087"] = 1000000, -- Runed Orb
+	["i:47556"] = 1250000, -- Crusader Orb
+	["i:49908"] = 1500000, -- Primordial Saronite
+	["i:40533"] = 50000, -- Walnut Stock
+	["i:30183"] = 700000, -- Nether Vortex
+	["i:35948"] = 16000, -- Savory Snowplum
+	["i:58278"] = 16000, -- Tropical Sunfruit
+	["i:39684"] = 9000, -- Hair Trigger
+	["i:34249"] = 1000000, -- Hula Girl Doll
+	["i:38426"] = 30000, -- Eternium Thread
+	["i:23572"] = 500000, -- Primal Nether
+	["i:27860"] = 6400, -- Purified Draenic Water
+	["i:35949"] = 8500, -- Tundra Berries
+	["i:18567"] = 30000, -- Elemental Flux
+	["i:90146"] = 20000, -- Tinker's Kit
+	["i:4342"] = 2500, -- Purple Dye
+	["i:10290"] = 2500, -- Pink Dye
+	["i:10647"] = 2000, -- Engineer's Ink
+	["i:14341"] = 5000, -- Rune Thread
+	["i:34412"] = 1000, -- Sparkling Apple Cider
+	["i:2325"] = 1000, -- Black Dye
+	["i:8343"] = 2000, -- Heavy Silken Thread
+	["i:2595"] = 2000, -- Jug of Badlands Bourbon
+	["i:6261"] = 1000, -- Orange Dye
+	["i:3857"] = 500, -- Coal
+	["i:4291"] = 500, -- Silken Thread
+	["i:11291"] = 4500, -- Star Wood
+	["i:2594"] = 1500, -- Flagon of Dwarven Mead
+	["i:3466"] = 2000, -- Strong Flux
+	["i:4340"] = 350, -- Gray Dye
+	["i:4341"] = 500, -- Yellow Dye
+	["i:4400"] = 2000, -- Heavy Stock
+	["i:2321"] = 100, -- Fine Thread
+	["i:6530"] = 100, -- Nightcrawlers
+	["i:2593"] = 150, -- Flask of Stormwind Tawny
+	["i:2596"] = 120, -- Skin of Dwarven Stout
+	["i:2605"] = 100, -- Green Dye
+	["i:2604"] = 50, -- Red Dye
+	["i:4289"] = 50, -- Salt
+	["i:4399"] = 200, -- Wooden Stock
+	["i:6260"] = 50, -- Blue Dye
+	["i:1179"] = 125, -- Ice Cold Milk
+	["i:2320"] = 10, -- Coarse Thread
+	["i:2324"] = 25, -- Bleach
+	["i:2678"] = 10, -- Mild Spices
+	["i:2880"] = 100, -- Weak Flux
+	["i:4470"] = 38, -- Simple Wood
+	["i:4537"] = 125, -- Tel'Abim Banana
+	["i:6217"] = 124, -- Copper Rod
+	["i:17194"] = 10, -- Holiday Spices
+	["i:17202"] = 10, -- Snowball
+	["i:30817"] = 25, -- Simple Flour
+	["i:44835"] = 10, -- Autumnal Herbs
+	["i:2901"] = 81, -- Mining Pick
+	["i:7005"] = 82, -- Skinning Knife
+	["i:159"] = 25, -- Refreshing Spring Water
+	["i:3371"] = 100, -- Crystal Vial
+	["i:5956"] = 18, -- Blacksmith Hammer
+	["i:17196"] = 50, -- Holiday Spirits
+	["i:39354"] = 15, -- Light Parchment
+	["i:44853"] = 25, -- Honey
+	["i:44854"] = 25, -- Tangy Wetland Cranberries
+	["i:44855"] = 25, -- Teldrassil Sweet Potato
+	["i:46784"] = 25, -- Ripe Elwynn Pumpkin
+	["i:46793"] = 25, -- Tangy Southfury Cranberries
+	["i:46796"] = 25, -- Ripe Tirisfal Pumpkin
+	["i:46797"] = 25, -- Mulgore Sweet Potato
+	["i:79740"] = 23, -- Plain Wooden Staff
 }
 
 -- returns the vendor cost for a given target item
 function TSMAPI:GetVendorCost(itemString)
+	itemString = TSMAPI:GetItemString2(itemString)
 	return itemString and TSM.db.global.vendorItems[itemString] or vendorItems[itemString]
 end
 
 function Vendor:ScanMerchant(first)
 	for i=1, GetMerchantNumItems() do
-		local itemString = TSMAPI:GetItemString(GetMerchantItemLink(i))
+		local itemString = TSMAPI:GetItemString2(GetMerchantItemLink(i))
 		if itemString then
 			local _, _, price, _, numAvailable, _, extendedCost = GetMerchantItemInfo(i)
 			if price > 0 and not extendedCost and numAvailable == -1 then
