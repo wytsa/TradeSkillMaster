79630c0aac6626f1449d572cfda859300427ba2b
Bart39 <bart39@sky.com>
2015-04-15 11:40:34 +0100

    Fixed issue #50

--- CoreAPI/Operations.lua
+++ CoreAPI/Operations.lua
@@ -21,7 +21,8 @@ local private = {operationInfo=TSM.moduleOperationInfo}
 function TSMAPI.Operations:GetFirstByItem(itemString, module)
 	TSMAPI:Assert(itemString and module, "Invalid parameters to TSMAPI.Operations:GetFirstByItem(...)")
 	local groupPath = TSM.db.profile.items[itemString]
-	if not groupPath or not TSM.db.profile.groups[groupPath][module] then return end
+	if not groupPath then return end
+	if not TSM.db.profile.groups[groupPath][module] then return end
 
 	for _, operation in ipairs(TSM.db.profile.groups[groupPath][module]) do
 		if operation ~= "" and not TSM.Modules:IsOperationIgnored(module, operation) then
