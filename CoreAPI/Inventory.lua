0e1767e8954faf9eee5e28d8309fab036112ff17
Sapu94 <sapu94@gmail.com>
2015-04-24 16:49:45 -0700

    Revert "Reagent bank no longer counts as bags for crafting inventory."
    This reverts commit eabd616461f4787fea215540c4f3218feffd07f9.

--- CoreAPI/Inventory.lua
+++ CoreAPI/Inventory.lua
@@ -234,7 +234,15 @@ function TSMAPI.Inventory:GetCraftingTotals(ignoreCharacters, otherItems)
 				total[itemString] = (total[itemString] or 0) + quantity
 			end
 			for itemString, quantity in pairs(data.reagentBank) do
-				otherTotal[itemString] = (otherTotal[itemString] or 0) + quantity
+				if player == PLAYER_NAME then
+					if otherItems[itemString] then
+						otherTotal[itemString] = (otherTotal[itemString] or 0) + quantity
+					else
+						bagTotal[itemString] = (bagTotal[itemString] or 0) + quantity
+					end
+				else
+					otherTotal[itemString] = (otherTotal[itemString] or 0) + quantity
+				end
 				total[itemString] = (total[itemString] or 0) + quantity
 			end
 			for itemString, quantity in pairs(data.mail) do
