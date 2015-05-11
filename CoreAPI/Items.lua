cd95d30d9c0e2fc36523a1e86d706d189a58cc03
Craig Gambino <cmgambino@gmail.com>
2015-05-11 14:46:08 -0700

    Add quality check

--- CoreAPI/Items.lua
+++ CoreAPI/Items.lua
@@ -297,7 +297,7 @@ end
 
 function TSMAPI.Item:IsDisenchantable(itemString)
 	if not itemString or TSM.STATIC_DATA.notDisenchantable[itemString] then return end
-	local quality, iType, TSMAPI.Util:Select({3, 6}, TSMAPI.Item:GetInfo(itemString))
+	local quality, iType = TSMAPI.Util:Select({3, 6}, TSMAPI.Item:GetInfo(itemString))
 	return quality >= 2 and (iType == ARMOR or iType == WEAPON)	
 end
 
