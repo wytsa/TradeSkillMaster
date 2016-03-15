734b8f34fa882c66c29f8e8ef367c67fd1ce82e5
Sapu94 <sapu94@gmail.com>
2016-03-14 20:32:04 -0700

    Only showing update notification once every 48 hours.

--- CoreAPI/Modules.lua
+++ CoreAPI/Modules.lua
@@ -197,9 +197,9 @@ function Modules:OnEnable()
 				local fullName = gsub(obj.name, "TSM_", "TradeSkillMaster_")
 				local currentVersion = private:VersionStrToInt(obj._version)
 				if currentVersion and addonVersions[fullName] and currentVersion < addonVersions[fullName] then
-					if not TSM.db.global.pendingAddonUpdate[fullName] then
+					TSM.db.global.pendingAddonUpdate[fullName] = TSM.db.global.pendingAddonUpdate[fullName] or time()
+					if TSM.db.global.pendingAddonUpdate[fullName] + 48 * 60 * 60 < time() then
 						TSM.db.global.pendingAddonUpdate[fullName] = time()
-					elseif TSM.db.global.pendingAddonUpdate[fullName] + 48 * 60 * 60 > time() then
 						StaticPopupDialogs["TSMUpdatePopup_"..fullName] = {
 							text = format(L["|cffffff00Important Note:|r An update is available for %s. You should update as soon as possible to ensure TSM continues to function properly."], fullName),
 							button1 = L["I'll Go Update!"],
