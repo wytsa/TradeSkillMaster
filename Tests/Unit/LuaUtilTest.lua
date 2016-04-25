8bc677ec212571583a1c58fea55bd74d0957894e
Sapu94 <sapu94@gmail.com>
2016-04-24 22:38:45 -0700

    Updated unit test.

--- Tests/Unit/LuaUtilTest.lua
+++ Tests/Unit/LuaUtilTest.lua
@@ -1,12 +1,10 @@
--- Unit testing starts
-EXPORT_ASSERT_TO_GLOBALS = true
-package.path = package.path .. ";/root/scripts/testing/fakes/?.lua"
-package.path = package.path .. ";/root/beta_addon_repos/TradeSkillMaster/CoreAPI/?.lua"
-require('wow-lua')
-require('wow-api')
-require('main')
-require('LuaUtil')
+-- boilerplate requires
+package.path = package.path .. ";" .. os.getenv("TSM_FAKES_PATH") .. "?.lua"
+require('tsmunit')
 luaunit = require('luaunit')
+
+-- code under test
+require('CoreAPI.LuaUtil')
 
 
 TestRound = {}
@@ -28,6 +26,7 @@ function TestRound:testSingleParameter()
     luaunit.assertEquals(TSMAPI.Util:Round(1.6), 2)
 end
 
+
 TestWipeOrCreateTable = {}
 function TestWipeOrCreateTable:testCreate()
 	luaunit.assertItemsEquals(TSMAPI.Util:WipeOrCreateTable(nil), {})
@@ -37,10 +36,12 @@ function TestWipeOrCreateTable:testWipe()
 	luaunit.assertItemsEquals(TSMAPI.Util:WipeOrCreateTable({1,2,"3","4"}), {})
 end
 
+
 TestSafeStrSplit = {}
 function TestSafeStrSplit:TestSplit()
 	luaunit.assertItemsEquals(TSMAPI.Util:SafeStrSplit("a,b,c,d",","), {"a","b","c","d"})
 end
+
 
 TestSelect = {}
 function TestSelect:TestNumber()
@@ -50,5 +51,6 @@ end
 function TestSelect:TestTable()
 	luaunit.assertItemsEquals({TSMAPI.Util:Select({1,3,5}, "a","b","c","d","e")}, {"a","c","e"})
 end
+
 
 os.exit(luaunit.LuaUnit.run())
